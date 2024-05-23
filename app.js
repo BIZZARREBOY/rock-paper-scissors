11593d55b3557a7560eeab9e9983ddfe9e54b2ac
bizzarreboy <luigod99@gmail.com>
2024-05-23 00:57:51 -0600

    Add score and break app when score its equals to five

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let computerScore = 0;
 let humanScore = 0;
-let fiveGames = 0;
+const main = document.querySelector("main");
+const result = document.getElementById("result");
+const player = document.getElementById("player");
+const computer = document.getElementById("computer");
 
 function getComputerChoice() {
     const characters = ["rock", "paper", "scissors"];
@@ -11,44 +14,60 @@ function getComputerChoice() {
 
 function playGame(humanChoice, computerChoice) {
     computerChoice = getComputerChoice();
-    const main = document.querySelector("main");
-    const h2 = document.querySelector("h2");
 
     if (humanChoice === computerChoice){
-        h2.textContent = "It's a TIE!!!";
+        result.textContent = "It's a TIE!!!";
     } else {
         switch(humanChoice) {
             case "rock":
                 if (computerChoice == "paper") {
-                    h2.textContent = "You lose! Paper beats rock.";
+                    result.textContent = "You lose! Paper beats rock.";
                     computerScore++;
                     break;
                 } else {
-                    h2.textContent = "You win! Rock beats scissors.";
+                    result.textContent = "You win! Rock beats scissors.";
                     humanScore++;
                     break;
                 }
             case "paper":
                 if (computerChoice == "scissors") {
-                    h2.textContent = "You lose! Scissors beats paper.";
+                    result.textContent = "You lose! Scissors beats paper.";
                     computerScore++;
                     break;
                 } else {
-                    h2.textContent = "You win! Paper beats rock.";
+                    result.textContent = "You win! Paper beats rock.";
                     humanScore++;
                     break;
                 }
             case "scissors": 
                 if (computerChoice == "rock") {
-                    h2.textContent = "You lose! Rock beats scissors";
+                    result.textContent = "You lose! Rock beats scissors";
                     computerScore++;
                     break;
                 } else {
-                    h2.textContent = "You win! Scissors beats paper";
+                    result.textContent = "You win! Scissors beats paper";
                     humanScore++;
                     break;
                 }
         }
     }
-    main.appendChild(h2);
+
+    player.textContent = `Player: ${humanScore}`;
+    computer.textContent = `Computer: ${computerScore}`;
+
+    if(humanScore === 5) {
+        result.textContent = "You win!!!";
+        main.appendChild(result);
+        result.remove();
+        computerScore = 0;
+        humanScore = 0;
+    } else if (computerScore === 5){
+        result.textContent = "You loose";
+        main.appendChild(result);
+        result.remove();
+        computerScore = 0;
+        humanScore = 0;
+    } else {
+        main.appendChild(result);
+    }
 }
